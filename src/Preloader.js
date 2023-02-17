6e675c895e6c40100bf57d735dfacd16f386e65a
tertiux44 <secondsonofadebayo@gmail.com>
2023-02-18 00:12:52 +0100

    version

--- src/Preloader.js
+++ src/Preloader.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Preloader(){
     return(
         <div className="preloader">
+            <div className="version">v 1.0.0</div>
             <div className="preloader-circle"></div>
         </div>
     )
